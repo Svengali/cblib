c471cb8331efb484cc8dfc692f594bf156125b74
Marc Hernandez <marc.hernandez@disruptivegames.com>
2021-05-23 13:08:03 -0700

    Switch to (1 << n) style

--- SegmentTriangle.h
+++ SegmentTriangle.h
@@ -6,13 +6,13 @@
 START_CB
 
 // masks for the edgeVertCheckFlags :
-#define LSSTRI_MASK_EDGE0	1
-#define LSSTRI_MASK_EDGE1	2
-#define LSSTRI_MASK_EDGE2	4
-#define LSSTRI_MASK_VERT0	8
-#define LSSTRI_MASK_VERT1	16
-#define LSSTRI_MASK_VERT2	32
-#define LSSTRI_MASK_ALL		63
+#define LSSTRI_MASK_EDGE0 (1 << 0) // 1
+#define LSSTRI_MASK_EDGE1	(1 << 1) // 2
+#define LSSTRI_MASK_EDGE2	(1 << 2) // 4
+#define LSSTRI_MASK_VERT0	(1 << 3) // 8
+#define LSSTRI_MASK_VERT1	(1 << 4) // 16
+#define LSSTRI_MASK_VERT2	(1 << 5) // 32
+#define LSSTRI_MASK_ALL		((1 << 6) - 1) // 63
 
 bool SegmentTriangleIntersect(const Segment& seg, SegmentResults* pResults,
 								const Vec3 &v0,
